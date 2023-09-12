6126ebf62dff6b89c517680e7e8418a2435c4384
Phillip <diphillipophillip@gmail.com>
2023-09-12 09:52:07 -0400

    most-of-landing

--- commune.io/src/app/app.component.ts
+++ commune.io/src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  title = "somethin"
 
 }
