d443d6a05f5115e089ac7baa4143dbc500706b1b
diphillipophillip <38082718+diphillipophillip@users.noreply.github.com>
2023-12-07 12:18:59 -0500

    Profile changes

--- commune.io-react/src/Components/Dashboard/Home/Profile/Profile.tsx
+++ commune.io-react/src/Components/Dashboard/Home/Profile/Profile.tsx
@@ -4,10 +4,39 @@ import IContainer from "../../../../Library/Container/IContainer";
 import IPanel from "../../../../Library/Panel/IPanel";
 import IAttributeBar from "../../../../Library/AttributeBar/IAttributeBar";
 import { attributeColors } from "../Constants/HomeConstats";
+import { BadgeCheckIcon } from "@heroicons/react/solid";
 
 function Profile({ setViewProfile, user }: any) {
   const handleSetViewProfile = (): void => {
     setViewProfile(false);
+  };
+
+  const calculateRegistrationAge = (registrationDate: any) => {
+    const regDate: any = new Date(registrationDate);
+    const today: any = new Date();
+    const diffTime = Math.abs(today - regDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    const weeksOld = Math.floor(diffDays / 7);
+    const monthsOld = Math.floor(diffDays / 30); // Approximation
+    const yearsOld = today.getFullYear() - regDate.getFullYear();
+
+    if (yearsOld > 0) return `${yearsOld} year(s) old`;
+    if (monthsOld > 0) return `${monthsOld} month(s) old`;
+    return `${weeksOld} week(s) old`;
+  };
+
+  const calculateAge = (dob: any) => {
+    const birthday = new Date(dob);
+    const today = new Date();
+    let age = today.getFullYear() - birthday.getFullYear();
+    const m = today.getMonth() - birthday.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
+      age--;
+    }
+
+    return age;
   };
 
   return (
@@ -17,7 +46,7 @@ function Profile({ setViewProfile, user }: any) {
         <IPanel height="h-full" marginTop="mt-8">
           <div className="px-12 py-6 ">
             <div className="flex">
-              <div className="w-[136px] h-[136px] rounded-full overflow-hidden">
+              <div className="w-[136px] h-[136px] rounded-full overflow-hidden flex">
                 <img
                   src="levelUp.png"
                   alt="Avatar"
@@ -25,8 +54,18 @@ function Profile({ setViewProfile, user }: any) {
                 />
               </div>
               <div className="ml-6 grid grid-rows">
-                <div className="text-2xl">
-                  {user?.firstName} {user?.lastName}
+                <div className="text-2xl flex justify-between">
+                  <div>
+                    <div className="mr-2">
+                      {user?.firstName} {user?.lastName}
+                    </div>
+
+                    <div className="text-xs" style={{ color: "#40B87E" }}>
+                      {user?.subscription?.tier}
+                    </div>
+                  </div>
+
+                  <div>{user?.userPoints}</div>
                 </div>
                 <div className="text-sm">
                   Reputation Score - {user?.reputation}
@@ -34,7 +73,12 @@ function Profile({ setViewProfile, user }: any) {
                 <div className="text-sm">
                   {user?.city}, {user.state}{" "}
                   <div className="text-sm">
-                    {user?.dateOfBirth} - {user?.gender}
+                    {calculateAge(user?.dateOfBirth)} - {user?.gender}
+                  </div>
+                </div>
+                <div className="text-sm">
+                  <div className="text-sm">
+                    {calculateRegistrationAge(user?.metadata?.registrationDate)}
                   </div>
                 </div>
               </div>
